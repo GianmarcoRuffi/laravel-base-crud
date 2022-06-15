dc1c52258b080a762eac07c93612c1acfb9b4554
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-06-15 17:55:43 +0200

    Ricreata tabella

--- database/migrations/2022_06_15_152112_update_comic_table.php
+++ database/migrations/2022_06_15_152112_update_comic_table.php
@@ -13,8 +13,21 @@ class UpdateComicTable extends Migration
      */
     public function up()
     {
-        Schema::table('comic_table', function (Blueprint $table) {
-            //
+        // Schema::table('comic_table', function (Blueprint $table) {
+        //     //
+        // });
+
+        
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->text('description');
+            $table->string('image');
+            $table->float('price', 5, 2);
+            $table->string('series', 150);
+            $table->date('sale_date');
+            $table->string('type', 100);
+            $table->timestamps();
         });
     }
 
@@ -25,8 +38,10 @@ class UpdateComicTable extends Migration
      */
     public function down()
     {
-        Schema::table('comic_table', function (Blueprint $table) {
-            //
-        });
+        // Schema::table('comic_table', function (Blueprint $table) {
+        //     //
+        // });
+        Schema::dropIfExists('comic');
+
     }
 }
