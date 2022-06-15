c20eba7dc440423f8d527366ca401caf1adac719
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-06-15 17:15:34 +0200

    Ciclo seeder

--- database/seeds/ComicTableSeeder.php
+++ database/seeds/ComicTableSeeder.php
@@ -11,6 +11,17 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+        foreach($comics as $comic){
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->image = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        }
     }
 }
